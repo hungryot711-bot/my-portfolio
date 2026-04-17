d006831794c08272d6288e467ca4af5424f6f0f2
丹生谷 拳志郎 <k-nyunoya@kawanet.ac.jp>
2026-04-17 12:24:28 +0900

    fix: 404エラー対策のためベースパスとfaviconのパスを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // ★ここを自分のリポジトリ名に合わせて追加！
+  base: '/my-portfolio/', // ★必ずリポジトリ名と同じ名前を / で囲んで入れてください
 })
